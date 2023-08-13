20a61fa3cbb2588ef3e75deb1c47a443c5ee63c2
Gustavo Vieira <76416937+GustavoOfSmach@users.noreply.github.com>
2023-08-12 21:18:09 -0300

    Add files via upload

---
+++ aula 10-08.py
@@ -0,0 +1,9 @@
+# Teste simples Usando input para o usuario inserir um valor para a função responder
+
+mensagem = 'Olá, '
+nome = input('Escreva seu nome aqui:')
+
+mensagem_nova = mensagem + nome
+print(mensagem_nova)
+
+
