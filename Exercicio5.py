6f4f336b2d687bd0109becd4b840a8e3eb10b11b
Gustavo Vieira <76416937+GustavoOfSmach@users.noreply.github.com>
2023-08-16 15:04:30 -0300

    Delete Exercicio5.py

--- Exercicio5.py
+++
@@ -1,6 +0,0 @@
-#EXERCICIO 5: Converter temperatura:Peça ao usuário para digitar uma temperatura em graus Celsius, depois deste
-#passo converta essa temperatura para Fahrenheit usando a fórmula: Fahrenheit = (Celsius * 9/5) + 32e apresentar a temperatura convertida na tela.
-
-celsius = float(input("Digite a temperatura em graus Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(f"A temperatura em Fahrenheit é {fahrenheit} graus.")
